0cf3b277f872d14c73fb1b8778ba77d361e526b0
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2024-02-02 00:32:34 +0100

    about me updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,29 @@
+import { FormattedMessage } from "react-intl";
 import man from "/man.png";
 
 const About = () => {
   return (
     <section className="relative mt-0" id="about">
-      <div className="flex lg:flex-row flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center lg:flex-row">
         <img
           src={man}
           alt="Man"
-          className="w-full lg:w-1/2 z-[3] mt-52"
+          className="z-[3] mt-52 w-full lg:w-1/2"
           draggable={false}
         />
-        <div className="w-full lg:w-1/2 p-4 lg:p-8">
-          <p className="text-text text-[16px] md:text-[18px] lg:text-[20px] lg:text-left text-center">
-            Lorem ipsum dolor sit amet consectetur. Non diam venenatis enim enim
-            nibh adipiscing malesuada ac interdum. Amet pellentesque integer
-            pharetra amet gravida maecenas eget lacus nulla. Congue pharetra
-            pretium porttitor bibendum duis velit enim nibh adipiscing malesuada
-            ac interdum. Amet pellentesque integer pharetra amet gravida
-            maecenas eget lacus nulla. Congue pharetra pretium porttitor
-            bibendum duis velit
+        <div className="lg:p-46 xl:mt-22 w-full p-4 lg:w-1/2">
+          <p className="max-w-[700px] whitespace-pre-line text-center text-[16px] text-text md:text-[18px] lg:text-left lg:text-[20px]">
+            <FormattedMessage
+              id="about.description"
+              values={{
+                travaux: (words) => (
+                  <a className="worklink" href="#works">
+                    {words}
+                  </a>
+                ), // Pour surligner
+                highlight: (words) => <span className="accent">{words}</span>, // Pour surligner
+              }}
+            />
           </p>
         </div>
       </div>
---
+++ src/components/Skills.jsx
@@ -0,0 +1,5 @@
+const Skills = () => {
+  return <div id="#skills">skills</div>;
+};
+
+export default Skills;
